ba9f4a57998091c06794a1197ac796097c578886
Enrique Ramirez <enrique@prsoft.com>
2020-12-24 10:00:29 -0400

    Upgraded to Fable 3.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./src/App.fsproj",
+    entry: "./.genJs/App.js",
     output: {
         path: path.join(__dirname, "./public"),
         filename: "bundle.js",
@@ -18,9 +18,6 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.fs(x|proj)?$/,
-            use: "fable-loader"
-        }, {
             test: /\.css$/,
             use: [
                 'style-loader',
